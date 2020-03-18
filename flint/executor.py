5f2ce5c21e3f41fbf4e752f025d7d704a8c93d41
Gaoxin Dai <daigx1990@gmmail.com>
2020-03-18 13:36:17 -0700

    Add ExecutorException

--- flint/executor.py
+++ flint/executor.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
-from handler import Handler
+from handler.handler_helper import Handler
+from handler.flow_data import FlowDataException
 import socket
-import os
 
 application = Flask(__name__)
 global_workflows = {}
@@ -19,29 +19,55 @@ class App:
     @staticmethod
     @application.route('/execute')
     def execute():
-        step = request.args.get('step')
-        workflow = request.args.get('workflow')
-        obj_name = request.args.get('obj_name')
-        handler_instance = Handler()
-        handler_instance.flow_data.obj_name = obj_name
-        try:
-            global_workflows[workflow][step](handler_instance)
+        missing_args = []
+        step = request.args.get('step', default=None)
+        workflow = request.args.get('workflow', default=None)
+        obj_name = request.args.get('obj_name', default=None)
+        if step is None:
+            missing_args.append("step")
+        if workflow is None:
+            missing_args.append("workflow")
+        if obj_name is None:
+            missing_args.append("workflow")
+        if len(missing_args) > 0:
+            message = "Flint Python Executor API Missing params: {}".format(', '.join(missing_args))
             response = {
-                "message": "",
-                "status": "success"
-            }
-        except Exception as err:
-            response = {
-                "message": err,
+                "message": message,
                 "status": "failure"
             }
+        else:
+            handler_instance = Handler()
+            handler_instance.flow_data.obj_name = obj_name
+            try:
+                try:
+                    global_workflows[workflow][step](handler_instance)
+                    response = {
+                        "message": "",
+                        "status": "success"
+                    }
+                except FlowDataException as e:
+                    response = {
+                        "message": e.reason,
+                        "status": "failure"
+                    }
+            except Exception as err:
+                response = {
+                    "message": err,
+                    "status": "failure"
+                }
         return jsonify(response)
 
     @staticmethod
     def start():
-        port = select_port()
-        write_port_to_file(port)
-        application.run(host='0.0.0.0', port=port)
+        try:
+            port = select_port()
+            write_port_to_file(port)
+            application.run(host='0.0.0.0', port=port)
+        except ExecutorException as e:
+            raise ExecutorException(status=e.status, reason=e.reason)
+        except Exception as e:
+            print(e)
+            raise ExecutorException(status=0, reason="Failed to start python executor api service")
 
 
 def create_app():
@@ -49,8 +75,12 @@ def create_app():
 
 
 def is_port_in_use(port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(('localhost', port)) == 0
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return s.connect_ex(('localhost', port)) == 0
+    except Exception as e:
+        print(e)
+        raise ExecutorException(status=0, reason="Failed to check available Port")
 
 
 def select_port():
@@ -58,11 +88,30 @@ def select_port():
     while True:
         if is_port_in_use(port):
             port += 1
+            if port > 8180:
+                raise ExecutorException(status=0, reason="Failed to find available Port between 8080 and 8180")
         else:
             break
     return port
 
 
+# todo change to working dir when deploy
 def write_port_to_file(port):
-    with open('/tmp/flint_python_executor_port', 'w') as writer:
-        writer.write(str(port))
+    file_path = '/tmp/flint_python_executor_port'
+    try:
+        with open(file_path,  'w') as writer:
+            writer.write(str(port))
+    except Exception as e:
+        print(e)
+        raise ExecutorException(status=0, reason="Failed to write port to file {0}".format(file_path))
+
+
+class ExecutorException(Exception):
+
+    def __init__(self, status=None, reason=None):
+        self.status = status
+        self.reason = reason
+
+    def __str__(self):
+        error_message = "Reason: {0}\n".format(self.reason)
+        return error_message
